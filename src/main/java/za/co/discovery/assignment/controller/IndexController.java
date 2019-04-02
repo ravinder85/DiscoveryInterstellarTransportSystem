488ee286badb18f63a2cf0db08ce07595c021d7c
ravinder85 <srreddy2003@gmail.com>
2019-04-02 09:35:37 +0200

    add the code for Discover project

--- src/main/java/za/co/discovery/assignment/controller/IndexController.java
+++ src/main/java/za/co/discovery/assignment/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController implements ErrorController {
--- src/main/java/za/co/discovery/assignment/controller/RootController.java
+++ src/main/java/za/co/discovery/assignment/controller/RootController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import za.co.discovery.assignment.entity.Edge;
 import za.co.discovery.assignment.entity.Traffic;
 import za.co.discovery.assignment.entity.Vertex;
